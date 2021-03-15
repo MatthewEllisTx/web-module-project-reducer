a5256bf940dedc410e20b976ea8ce75c005a9767
Matthew Ellis <mbellistx@gmail.com>
2021-03-15 17:26:26 -0500

    mvp done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY';
+export const ADD_TO_MEM = 'ADD_TO_MEM';
+export const USE_MEM = 'USE_MEM';
+export const CLEAR_MEM = 'CLEAR_MEM';
 
 export const addOne = () => {
     return({type:ADD_ONE});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, ADD_TO_MEM, USE_MEM, CLEAR_MEM } from './../actions';
 
 export const initialState = {
     total: 100,
@@ -36,6 +36,30 @@ const reducer = (state, action) => {
                 ...state,
                 operation: action.payload
             });
+
+        case(CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
+            });
+
+        case(ADD_TO_MEM):
+            return ({
+                ...state,
+                memory: state.total
+            })
+
+        case(USE_MEM):
+            return ({
+                ...state,
+                total: calculateResult(state.total, state.memory, state.operation)
+            })
+        
+        case(CLEAR_MEM):
+            return({
+                ...state,
+                memory: 0
+            })
             
         default:
             return state;
